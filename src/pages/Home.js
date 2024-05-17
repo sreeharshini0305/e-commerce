c8edfcb2ae5e76236f0553974cd49614702e48bc
sreeharshini0305 <sreeharshinimandala@gmal.com>
2024-05-17 23:38:37 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,11 +9,11 @@ const Home = () => {
   // get products from product context
   const { products } = useContext(ProductContext);
   // get only men's and women's clothing category
-  const filteredProducts = products.filter((item) => {
-    return (
-      item.category === "men's clothing" || item.category === "women's clothing"
-    );
-  });
+  // const filteredProducts = products.filter((item) => {
+  //   return (
+  //     item.category === "men's clothing" || item.category === "women's clothing"
+  //   );
+  // });
 
   return (
     <div>
@@ -21,7 +21,7 @@ const Home = () => {
       <section className='py-10'>
         <div className='container mx-auto px-8'>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
-            {filteredProducts.map((product) => {
+            {products.map((product) => {
               return <Product product={product} key={product.id} />;
             })}
           </div>
