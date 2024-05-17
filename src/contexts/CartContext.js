92bc9261fe6b04387cc3855665f021aab6cf6918
sreeharshini0305 <sreeharshinimandala@gmal.com>
2024-05-17 16:05:21 +0530

    react js

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,93 @@
+import React,{createContext,useEffect,useState} from 'react'
+import { TbCubeUnfolded } from 'react-icons/tb';
+// create context
+export const CartContext=createContext();
+const CartProvider = ({children}) => {
+    // cart state
+    const [cart,setCart]=useState([]);
+    // item amount state
+    const [itemAmount,setItemAmount]=useState(0);
+    // total price state
+    const[total,setTotal]=useState(0);
+
+    useEffect(()=>{
+        const total=cart.reduce((accumulator,currentItem)=>{
+            return accumulator+currentItem.price*currentItem.amount;
+        },0)
+        setTotal(total);
+    })
+    // update item amount
+    useEffect(()=>{
+        if(cart){
+            const amount=cart.reduce((accumulator,currecntItem)=>{
+                return accumulator+currecntItem.amount;
+            },0)
+            setItemAmount(amount);
+        }
+    },[cart]);
+    // add to cart
+    const addToCart=(product,id)=>{
+        const newItem={...product,amount:1}
+        const cartItem=cart.find((item)=>{
+            return item.id===id;
+        });
+            // if cart item is already in the cart
+        if(cartItem){
+            const newCart=[...cart].map((item)=>{
+                if(item.id===id){
+                    return {...item,amount: cartItem.amount+1};
+                }else{
+                    return item;
+                }
+            });
+            setCart(newCart)
+        }else{
+            setCart([...cart,newItem])
+        }
+    }
+// remove from cart
+const removeFromCart=(id)=>{
+    const newCart=cart.filter((item)=>{
+        return item.id!==id;
+    });
+    setCart(newCart);
+}
+// clear cart
+const clearCart=()=>{
+    setCart([]);
+}
+// increase amout
+const increaseAmount=(id)=>{
+    const item=cart.find((item)=>
+        { return item.id===id
+});
+addToCart(item,id);
+}
+
+// decrease amount
+const decreaseAmount=(id)=>{
+    const cartItem=cart.find((item)=>item.id===id);
+    if(cartItem){
+        const newCart=cart.map(item=>{
+            if(item.id===id){
+                return {...item,amount:cartItem.amount-1};
+            }else{
+                return item;
+            }
+        })
+        setCart(newCart);
+    }
+
+        if(cartItem.amount<2){
+            removeFromCart(id);
+        }
+
+}
+    return (
+        <CartContext.Provider value={{cart,addToCart,removeFromCart,clearCart,increaseAmount,decreaseAmount,itemAmount,total}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider
