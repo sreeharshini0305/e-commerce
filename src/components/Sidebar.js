4ea8099a7eaf0b2568d6a8cd9714d0f49459ce30
sreeharshini0305 <sreeharshinimandala@gmal.com>
2024-05-17 17:10:05 +0530

    react project

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,13 +16,13 @@ const{cart,clearCart,total,itemAmount}=useContext(CartContext);
     return (
         <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
             <div className='flex items-centre justify-between py-6 border-b'>
-                <div className='uppercase text-sm font-semibold'>Shopping Bag({itemAmount})</div>
+                <div className='uppercase text-medium font-semibold justify-center items-center'>Shopping Bag({itemAmount})</div>
             {/* icon */}
             <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
                 <IoMdArrowForward className='text-2xl' />
             </div>
             </div>
-            <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b'>
+            <div className='flex flex-col gap-y-2 h-[500px] lg:h-[500px] overflow-y-auto overflow-x-hidden border-b'>
                 {cart.map((item)=>{
                 return <CartItem item={item} key={item.id}/>
             })} 
